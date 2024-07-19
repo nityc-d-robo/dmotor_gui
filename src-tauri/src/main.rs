7934ab8d0e442a83a7ab20d05357cec8769ac457
sarukiti <sarukiti1891@gmail.com>
2024-07-19 16:18:07 +0900

    デッドロックの可能性があるクソコードを排除

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -32,28 +32,29 @@ fn send_md(/*handle: tauri::State<DeviceHandle<GlobalContext>>, */command: MdStr
 #[tauri::command(async)]
 async fn send_blmd(blmd_process_manager: tauri::State<'_, Arc<Mutex<[i32; 8]>>>,handle: tauri::State<'_, DeviceHandle<GlobalContext>> ,command: BlMdStruct) -> Result<(),()>{
     let manager = blmd_process_manager.clone();
-    manager.lock().unwrap()[(command.controller_id - 1) as usize]+=1;
+    let mut manager = manager.lock().unwrap();
+    manager[(command.controller_id - 1) as usize]+=1;
     thread::sleep(Duration::from_millis(15));
     match command.mode {
         2 => {
-            while manager.lock().unwrap()[(command.controller_id - 1) as usize] == 1{
+            while manager[(command.controller_id - 1) as usize] == 1{
                 motor_lib::blmd::send_current(&handle, command.controller_id, command.value);
             }
-            manager.lock().unwrap()[(command.controller_id - 1) as usize]-=1;
+            manager[(command.controller_id - 1) as usize]-=1;
         },
         3 => {
             let config = PidConfig::new(1.0, 0.1, 0.1).with_limits(-16384.0, 16384.0);
             let mut pid = VelPid::new(config);
-            while manager.lock().unwrap()[(command.controller_id - 1) as usize] == 1{
+            while manager[(command.controller_id - 1) as usize] == 1{
                 println!("{}", command.value);
                 motor_lib::blmd::send_velocity(&handle, &mut pid, command.controller_id, command.value);
                 thread::sleep(Duration::from_millis(10));
             }
-            manager.lock().unwrap()[(command.controller_id - 1) as usize]-=1;
+            manager[(command.controller_id - 1) as usize]-=1;
         },
         _ => {
             motor_lib::blmd::receive_status(&handle, command.controller_id);
-            manager.lock().unwrap()[(command.controller_id - 1) as usize]-=1;
+            manager[(command.controller_id - 1) as usize]-=1;
         },
     };
     Ok(())
